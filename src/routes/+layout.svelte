6516fb5c48f185ed89e51c8cebc43e572cd152a5
PG Openda <piusopenda>
2024-07-25 14:56:05 +0300

    Addition: Skeleton of the navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,16 @@
 </script>
 
 <header>
-    <nav class="navbar">
-        <img src="/Logo.svg" alt="">
+    <a href="/">
+        <img src="/Logo.svg" alt="">  
+    </a>
+    <nav class="navlinks">
         <a href="/"> Home </a>
-        <a href="/"> About</a>
+        <a href="/"> Who We Are</a>
+        <a href="/"> About Liver Cancer</a>
+        <a href="/"> Get Involved</a>
+        <a href="/"> News & Updates</a>
+        <a href="/" class="contact-btn"> Contact Us</a>
     </nav>
 </header>
 
@@ -19,5 +25,21 @@
 </footer>
 
 <style>
-    
+    header {
+        display: flex;
+        justify-content: space-between;
+        padding: 0% 1.5%;
+    }
+
+    nav {
+        margin-top: auto;
+        margin-bottom: auto;
+    }
+
+    nav a{
+        text-decoration: none;
+        font-size: 1.2em;
+        padding-right: 8px;
+        font-weight: 500;
+    }
 </style>
