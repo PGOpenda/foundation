75c2a96fb76a4cd0898665dd7a5bca9028316900
PG Openda <piusopenda>
2024-07-27 13:55:02 +0300

    Addition: Halfway done with about us

---
+++ src/routes/about-us/+page.svelte
@@ -0,0 +1,125 @@
+<div class="about-heading">
+    <div>
+        <h3>
+            All About the 
+        </h3>
+        <h1>
+            Liver Care <br>Foundation
+        </h1>
+    </div>
+    <div>
+        <p>An image goes here!!</p>
+    </div>
+</div>
+
+<div class="about-info">
+    <h2>
+        Our Vision: To create a future where no one has to face liver cancer alone, and where every individual 
+        can look forward to a healthy, hopeful, and vibrant life.
+    </h2>
+    <p>
+        Our vision is a world where liver cancer is a preventable and curable disease, where every patient has access 
+        to the best possible care, and where research and innovation continually push the boundaries of what is possible.
+    </p>
+
+    <h2>
+        Our mission: To advance the understanding, prevention, and treatment of liver cancer.
+    </h2>
+
+    <p> 
+        We aim to provide patients and their families with the necessary resources and support to navigate their journey,
+        while fostering a community of hope and resilience. Through innovative research, public education, and compassionate care,
+        we are committed to reducing the impact of liver cancer on individuals and society.
+    </p>
+</div>
+
+
+<div class="about-cards">
+    <h2>
+        Our Core Values
+    </h2>
+    <div class="cards">
+        <div style="background-color: aqua;">
+            <p>journey</p>
+        </div>
+
+        <div style="background-color:black;">
+            <p>journey</p>
+        </div>
+
+        <div style="background-color:blue;">
+            <p>journey</p>
+        </div> 
+
+        <div>
+            journey
+        </div>
+    </div>
+</div>
+
+<style>
+    .about-heading{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        padding: 70px 100px;
+    }
+
+    .about-heading h3, .about-heading h1 {
+        font-weight: 400;
+    }
+
+    .about-heading h3{
+        font-size: 1.8em;
+        margin-bottom: 10px;
+    }
+
+    .about-heading h1{
+        font-size: 3.3em;
+    }
+
+    .about-info{
+        padding: 70px 100px;
+        background-color: #fff;
+        text-align: center;
+    }
+
+
+    .about-info h2, .about-info p {
+        display: inline-block;
+        max-width: 700px; /* Adjust this value as needed */
+        width: 100%;
+        margin: 0 auto;
+    }
+
+    .about-info h2{
+        color: #9d0063;
+        font-weight: 400;
+        font-size: 2.2em;
+        margin-bottom: 25px;
+    }
+
+    .about-info p{
+        margin-bottom: 90px;
+        text-align: left;
+    }
+
+    .about-cards{
+        padding: 70px 100px;
+        text-align: center;
+    }
+
+    .about-cards h2{
+        font-size: 2.2em;
+        font-weight: 400;  
+    }
+
+    .cards{
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        width: 100%;
+        padding: 30px;
+    }
+
+
+
+</style>
