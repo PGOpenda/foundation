61952216386c280d685278fe73b43e0126f0dd7f
PG Openda <piusopenda>
2024-07-28 22:05:45 +0300

    Completion: Liver Information done

--- src/routes/liver-cancer/+page.svelte
+++ src/routes/liver-cancer/+page.svelte
@@ -7,35 +7,199 @@
     </div>
 </div>
 
-<div class="links">
-    <div>
+<div class="info">
+
+    <div class="nav">
+        <ul>
+            <li><a href="/">About Liver Cancer</a></li>
+            <li><a href="/">Signs & Symptoms</a></li>
+            <li><a href="/">Risk factors and prevention</a></li>
+            <li><a href="/">Treatment Options</a></li>
+            <li><a href="/">Treatment Options</a></li>
+            <li></li>
+        </ul>
+    </div>
+
+    <div class="liver-info">
         <h2>
-            Find the necessary links below about liver cancer
+           What is liver cancer?
         </h2>
+        <p> 
+            Liver cancer (also known as hepatic cancer, primary hepatic cancer, or primary hepatic malignancy) is a type of cancer 
+            that begins in the liver. Liver cancer can be primary, meaning it begins in the liver. Hepatic metastasis, or secondary,
+            meaning it travels from another part of the body to the liver. Of the two types of liver cancer, liver metastasis is the 
+            most common. 
+        </p>
+
+        <h2>Classifications of Liver Cancer</h2>
+        <p>
+            In addition to the liver's parenchyma, additional components like the bile duct, blood arteries, and immune cells can also cause liver cancer.
+            <br>
+        </p>
+        <ol>
+            <li>
+                <strong>Hepatocellular carcinoma </strong> <br>
+                Hepatocellular carcinoma (HCC) is the most common type of liver cancer, making up around 75% of all primary liver malignancies . Hepatocytes, 
+                which are liver cells, can develop into cancerous cells to cause HCC.
+            </li>
+            <li>
+                <strong>Intrahepatic cholangiocarcinoma</strong> <br>
+                About 6% of primary liver tumors are bile duct malignancies, including cholangiocarcinoma and cholangiocellular cystadenocarcinoma. 
+                An epithelial malignancy of the intrahepatic biliary tree branches is known as intrahepatic cholangiocarcinoma (CCA). 
+                The second most common cause of primary liver cancer is intrahepatic CCA.
+            </li>
+            <li>
+                <strong>Angiosarcoma and hemangiosarcoma</strong> <br>
+                These are rare and aggressive liver tumors, yet they are the third most prevalent, accounting for 0.1-2.0% of all primary liver cancers. 
+                Angiosarcoma and hemangiosarcoma of the liver develop from the blood vessel's endothelial layer. These tumors have bad outcomes because 
+                they grow quickly and spread easily. 
+            </li>
+            <li>
+                <strong>Hepatoblastoma</strong> <br>
+                Hepatoblastoma is a type of cancer caused by immature liver cells. It is a rare malignant tumor that typically occurs in children, accounting 
+                for around 1% of all malignancies in children and 79% of all primary liver tumors under the age of fifteen.
+            </li>
+            <li>
+                <strong>Metastasis to liver </strong> <br>
+                Many malignancies seen in the liver are not real liver cancers, but rather cancers from other parts of the body that have metastasized to the liver.
+                 Frequently, the site of genesis is the digestive tract.
+            </li>
+        </ol>
+
+    </div>
+
+</div>
+
+<div class="causes">
+    <h2>
+        Causes of Liver Cancer
+    </h2>
+
+    <p>
+        Viral infection with hepatitis C virus (HCV) or Hepatitis B virus (HBV) is the chief cause of liver cancer in the world today, 
+        accounting for 80% of HCC
+    </p>
+
+    <div class="causes-list">
+        <div class="causes-list-1">
+            <ul>
+                <li>Viral infection</li>
+                <li>Aflatoxin</li>
+                <li>Obesity</li>
+                <li>Smoking</li>
+                <li>Liver fluke infection</li>
+            </ul>
+        </div>
+
+        <div class="causes-list-2">
+            <ul>
+                <li>Cirrhosis</li>
+                <li>High grade dysplastic nodules</li>
+                <li>Diabetes</li>
+                <li>Primary sclerosing cholangitis</li>
+                <li>Genetic conditions e.g. untreated hereditary hemochromatosis</li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="treatment">
+    <div>
+        <h2>Treatment for liver cancer</h2>
+        <p>
+            The kind and stage of the cancer, as well as your health, will determine how you are treated.
+        </p>
+    </div>
+
+    <div>
+        <h3>
+            General Consideration:
+        </h3>
+
+        <p>
+            Treatment for liver cancer varies according to its particular form and stage, just like with many other malignancies. 
+            The TNM staging systems serve as the primary basis for cancer staging. Additionally, there are staging systems tailored 
+            to liver cancer, and each one provides therapeutic options that could lead to a cure or prevention of cancer recurrence. <br> <br>
+            Surgery, medicine, and ablation techniques are used as treatments. For liver cancer, numerous chemotherapy medications are also approved.
+        </p>
+
+        <h3>
+            Hepatocellular carcinoma
+        </h3>
+
+        <p>
+            For individuals with sufficient hepatic function reserve, the recommended treatment for hepatocellular carcinoma 
+            (HCC) is partial surgical resection. Recurrence rates following excision, however, can surpass 70%, whether as a 
+            result of the original tumor spreading or developing new tumors. <br> <br>
+            When a patient has HCC and the tumor satisfies certain requirements, liver transplantation may also be an option 
+            (such as the Milan criteria). <br> <br>
+            The only non-surgical treatment that can provide a cure is percutaneous ablation. Percutaneous ablation can take 
+            many different forms. It can involve injecting substances like ethanol or acetic acid into the liver or employing 
+            radio frequency ablation, microwaves, lasers, or cryotherapy to produce dramatic temperature changes. <br> <br>
+            Despite the possibility of using local chemotherapy during a process called transarterial chemoembolization (TACE), 
+            systemic chemotherapy is not frequently employed in HCC. Treatment involves applying medications to the tumor that 
+            stop blood flow and destroy cancer cells.
+        </p>
+
+        <h3>
+            Intrahepatic cholangiocarcinoma
+        </h3>
+        <p>
+            Although resection is a possibility for cholangiocarcinoma, less than 30% of cases are treatable at the time of 
+            diagnosis. Many intrahepatic cholangiocarcinomas have many focal tumors within the liver, which makes surgical removal 
+            of the bulk of these tumors impossible. Following surgery, up to 60% of cases return. <br> <br>
+            In situations when partial resection is not a possibility, liver transplantation may be utilized; in other cases, 
+            adjuvant chemotherapy and radiation may be beneficial. <br> <br>
+            Since the perihilar area is where 60% of cholangiocarcinomas originate, photodynamic therapy can prolong survival and 
+            enhance quality of life in these incurable cases. A new kind of treatment called photodynamic therapy targets the tumor 
+            by using chemicals that are activated by light. <br> <br>
+            Cholangiocarcinoma instances that are incurable may occasionally be treated with systemic chemotherapy, such as gemcitabine
+             and cisplatin.
+        </p>
+
+        <h3>
+            Hepatoblastoma
+        </h3>
+        <p>
+            Hepatoblastoma can be treated by removing the tumor through surgical resection or liver transplantation. Occasionally, 
+            surgery can provide a remedy. Chemotherapy can be applied both prior to and following surgery or a transplant. <br> <br>
+            Chemotherapy for systemic hepatoblastoma treatment include vincristine, cyclophosphamide, doxorubicin, and cisplatin. 
+            Among these medications, cisplatin appears to be the most useful.
+         </p>
+
+         <h3>
+            Angiosarcoma and hemangiosarcoma
+         </h3>
+         <p>
+            Ultimately, a large number of these cancers are incurable. One course of treatment is to have the damaged liver sections 
+            surgically removed. Hemangiosarcomas and angiosarcomas of the liver do not respond well to chemotherapy or liver transplantation. 
+         </p>
+    </div>
+</div>
+
+<div class="analysis">
+    <h2>
+        Statistical Analysis
+    </h2>
+    <div>
+        <p>
+            According to the most recent epidemiological data, liver cancer ranks in the top ten for both prevalence and fatality. According to the 
+            Global Burden of Disease Liver Cancer Collaboration, new incidences of liver cancer grew by 75% each year between 1990 and 2015. 
+            According to the most recent data, 841,000 new liver cancer diagnoses and 782,000 deaths occur worldwide each year.
+        </p>
+        <div>
+            
+        </div>
     </div>
     
-    <div class="link-cards">
-        <div class="card">
-            <p>Link 1</p>
-        </div>
+</div>
 
-        <div class="card">
-            <p>Link 1</p>
-        </div>
-
-        <div class="card">
-            <p>Link 1</p>
-        </div>
-
-        <div class="card">
-            <p>Link 1</p>
-        </div>
-
-        <div class="card">
-            <p>Link 1</p>
-        </div>
-    </div>
-
+<div class="disclaimer">
+    <h1>
+        DISCLAIMER: <br> THE INFORMATION PROVIDED ON THIS SITE IS FOR INFORMATIONAL PURPOSES ONLY AND IS NOT INTENDED AS A SUBSTITUTE FOR PROFESSIONAL 
+        MEDICAL ADVICE, DIAGNOSIS, OR TREATMENT. ALWAYS SEEK THE ADVICE OF YOUR PHYSICIAN OR OTHER QUALIFIED HEALTH PROVIDER WITH ANY QUESTIONS 
+        YOU MAY HAVE REGARDING A MEDICAL CONDITION.
+    </h1>
 </div>
 
 <style>
@@ -49,28 +213,106 @@
         font-size: 3.3em;
     }
 
-    .links{
+    .info{
+        padding: 70px 100px;
+        background-color: #fff;
+    }
+
+    .nav{
+        border: 1px solid black;
+        border-radius: 10px;
+        
+    }
+
+    .nav ul{
+        list-style: none;
+        padding: 10px ;
+        display: flex ;
+        justify-content: center;
+    }
+
+    .nav ul li{
+        padding: 5px 15px;
+    }
+
+    .nav a{
+        color: black;
+    }
+
+    .nav a:hover{
+        color: #ff347d;
+        border-bottom: 1px solid #ff347d;
+        transition: 500ms;
+    }
+
+    .liver-info{
+        margin-top: 35px;
+    }
+
+    h2 , p, ol{
+        margin-bottom: 20px;
+    }
+
+    h2 {
+        font-size: 2em;
+    }
+
+    .liver-info ol {
+        line-height: 1.5em;
+        width: 80%;
+        margin-left: 20px;
+    }
+
+    .causes {
+        padding: 70px 100px;
+        /* text-align: center; */
+    }
+
+    .causes h2, .causes p{
+        text-align: center;
+    }
+
+    .causes-list{
+        width: 70%;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        /* align-items: center; */
+        margin-left: auto;
+        margin-right: auto;
+        border: 1px solid black;
+        border-radius: 10px;
+        padding: 50px 0px;
+    }
+
+    .causes-list li{
+        line-height: 2em;
+    }
+
+    .causes-list-1{
+        margin-left: 60px;
+    }
+
+    .causes-list-2{
+        margin-left: 30px;
+    }
+
+    .treatment{
+        padding: 70px 100px;
+        background-color: #fff;
+        
+    }
+
+    .treatment h3{
+        font-size: 1.5em;
+    }
+    
+    .analysis {
         padding: 70px 100px;
         text-align: center;
     }
 
-    .links h2{
-        font-size: 2em;
-        margin-bottom: 40px;
+    .disclaimer {
+        padding: 50px 100px;
+        color: red;
     }
-
-    .link-cards {
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr ;
-    }
-
-    .card {
-        width: 80%;
-        padding: 30px;
-        margin-bottom: 30px;
-        border-radius: 10px;
-        background-color: #9d0063;
-        color: #fff;
-    }
-
 </style>
